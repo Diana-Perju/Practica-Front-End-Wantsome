1881e09cf0d63faee3332934ab3575654aa16743
Diana-Perju <burghelea.diana@yahoo.com>
2019-07-25 00:23:58 +0300

    Completed Objects and This Homework

---
+++ JSObjectsandThis/JSObjectsandThis.js
@@ -0,0 +1,37 @@
+var calculator = {
+
+    a: 20,
+    b: 10,
+    
+    
+    read: function() {
+      this.a
+      this.b
+      
+    },
+    
+    sum: function() {
+      return this.a + this.b;
+    },
+    
+    diff: function() {
+      return this.a - this.b;
+    },
+    
+    mult: function() {
+    return this.a * this.b;
+    },
+    
+    divide: function() {
+    return this.a / this.b;
+    },
+    
+    }
+    
+    
+    console.log(calculator.read.bind());
+    console.log(calculator.sum());
+    console.log(calculator.diff());
+    console.log(calculator.mult());
+    console.log(calculator.divide());
+    
