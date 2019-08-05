502f93e5364ed3ff1da40c9c713ed91b18b66eaa
Diana-Perju <burghelea.diana@yahoo.com>
2019-08-05 22:52:41 +0300

    Completed DOM Homework part1

--- DOMHomework/DOMHomework.js
+++ DOMHomework/DOMHomework.js
@@ -3,18 +3,19 @@
 var chFont = document.querySelector("body");
 chFont.style.fontFamily = "Arial, sans-serif";
 
+
 // Schimbati fiecare span(name, fav-drink, born-place) cu informatiile despre voi
 var elemName = document.getElementById("name").innerHTML = "Diana Perju";
 var favDrink = document.getElementById("fav-drink").innerHTML = "Coca-Cola Cherry";
 var bornPlace = document.getElementById("born-place").innerHTML = "Bacau";
 
 // Iterati prin fiecare "li" si puneti clasa "listitem". Adaugati un tag style(in head) care seteaza o regula pentru .listitem sa aiba culoarea rosie
-
 var listItems = document.getElementsByTagName('li');
 for(var i = 0; i < listItems.length; i++) {
     listItems[i].classList.add("listitem");
     listItems[i].style.color = "red";
 }
+
 
 // Creati un element img si setati atributul src sa aiba o poza cu voi sau ceva random de pe net. Puneti acest element(append) la pagina noastra
 
